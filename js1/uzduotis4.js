94d87f2f4ce75b24a8ad890303f14e091b64da07
HG <82829500+hillagvar@users.noreply.github.com>
2024-03-22 19:25:58 +0100

    alternative solution

--- js1/uzduotis4.js
+++ js1/uzduotis4.js
@@ -5,12 +5,21 @@ Skaičius, sudarytas iš trijų skaitmenų, vadinamas Armstrongo skaičiumi, jei
 let pradzia = 100;
 let pabaiga = 999;
 
+// for (let i = pradzia; i <= pabaiga; i++) {
+//     let pirmas = (i - (i % 100))/100;
+//     let antras = ((i % 100) - (i % 10))/10;
+//     let trecias = i % 10;
+//     if (pirmas**3 + antras**3 + trecias**3 === i) {
+//         console.log(i);
+//     }
+// }
+
 for (let i = pradzia; i <= pabaiga; i++) {
-    let pirmas = (i - (i % 100))/100;
-    let antras = ((i % 100) - (i % 10))/10;
+    let pirmas = Math.floor(i/100);
+    let antras = Math.floor(i % 100 / 10);
     let trecias = i % 10;
     if (pirmas**3 + antras**3 + trecias**3 === i) {
-        console.log(i);
+    console.log(i);
     }
 }
 
