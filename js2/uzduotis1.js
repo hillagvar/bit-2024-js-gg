0db205686b4d4cf0fe74d07ace9fd82098b7909c
HG <82829500+hillagvar@users.noreply.github.com>
2024-03-23 13:01:36 +0100

    uzduotis2

--- js2/uzduotis1.js
+++ js2/uzduotis1.js
@@ -1,6 +1,11 @@
+// Faile pateiktos dienos oro temperatūros. Parašykite programą kuri surastų ir išvestų didžiausią ir mažiausią temperatūrą, bei dienas kuriomis buvo šios temperatūros.
+// Failo pavyzdys:
+// 1 2 7 3 4 6 -1 2 4 -3 -3 -5 1 5 6 7 5 9 2 3 6 7 8 6 3 6 7 5 6 7 1
+
+
 const fs = require("fs");
 
-let data = fs.readFileSync("temp.txt").toString().split(" ").map(Number);
+const data = fs.readFileSync("temp.txt").toString().split(" ").map(Number);
 
 // console.log(data);
 
---
+++ js2/uzduotis2.js
@@ -0,0 +1,19 @@
+// Faile pateikti skaičiai (sveikieji, iš intervalo nuo 0 iki 9), parašykite JS programą kuri suskaičiuotų kiek ir kokių skaičių yra tekstiniame faile.
+
+const fs = require("fs");
+
+const data = fs.readFileSync(process.argv[2]).toString().split(";").map(Number);
+
+// console.log(data);
+
+const numbers = Array(10).fill(0);
+
+console.log(numbers);
+
+data.forEach((x) => {
+    numbers[x]++;
+});
+
+numbers.forEach((n,i) => {
+    console.log(`Number: ${i}, repeats: ${n}`);
+});
