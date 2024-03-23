6cbf06f490f804960ce78e993f472469214e58b4
HG <82829500+hillagvar@users.noreply.github.com>
2024-03-23 13:31:34 +0100

    uzduotis3

---
+++ js2/uzduotis3.js
@@ -0,0 +1,42 @@
+// Faile pateikiami duomenys masyvas turintis 4,4 elementų. Pavyzdžiui:
+// 1 2 3 5
+// 5 4 4 7
+// 3 2 1 9
+// 2 3 4 5
+// Parašykite programą kuri apverstų (pasuktų) masyvą, rezultatas atvaizduojamas ekrane:
+// 1 5 3 2
+// 2 4 2 3
+// 3 4 1 4
+// 5 7 9 5
+
+const fs = require("fs");
+
+const data = fs.readFileSync(process.argv[2]).toString().split("\r\n");
+
+const mas = [];
+
+data.forEach((line) => {
+   mas.push(line.split(" "));
+});
+
+// console.log(mas);
+
+const newMas = [];
+
+for (let i = 0; i < 4; i++) {
+    let line = [];
+    for (let j = 0; j < 4; j++) {
+        line.push(mas[j][i]);
+    }
+    newMas.push(line);
+}
+
+// console.log(newMas);
+
+newMas.forEach((line) => {
+    let output = "";
+    line.forEach((x) => {
+        output += x + " ";
+    })
+    console.log(output);
+});
