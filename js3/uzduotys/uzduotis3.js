36b90bb4379194403de810b7ce8646e1748a6700
HG <82829500+hillagvar@users.noreply.github.com>
2024-03-23 16:46:49 +0100

    uzduotis2 ver1

---
+++ js3/uzduotys/uzduotis3.js
@@ -0,0 +1,9 @@
+// Garso signalas gali būti koduojamas sveikųjų skaičių seka. Šie skaičiai rodo signalo stiprumą periodiniais laiko intervalais. Signalą iškraipantis triukšmas šiek tiek pakeičia tų skaičių reikšmes.
+// Signalo „išlyginimo“ metu triukšmas pašalinamas tokiu būdu: kiekvienas skaičius keičiamas jo ir dviejų jam gretimų skaičių vidurkiu (vidutinės reikšmės sveikąja dalimi). Pirmas ir paskutinis skaičiai atitinkamai keičiami dviejų pirmųjų arba dviejų paskutinių skaičių vidurkiu.
+// Faile yra garso signalo seka, kuri sudaryta iš k nuoskaitų. Pirmojoje failo eilutėje yra pateikiami skaičius k. Toliau surašytos visa seka. Sudarykite programą kuri nufiltruotų šias sekas ir išvestų jas į ekraną.
+// Pradiniai duomenys
+// 4 7 3 5 8
+
+// Rezultatai
+// 5.5 4.6 5 5.3 6.5
+
