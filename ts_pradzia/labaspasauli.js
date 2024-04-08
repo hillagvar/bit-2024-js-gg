d737644a1d4c6c5704451477a47454702d326a99
Hila Gvar <hilla.gvar@gmail.com>
2024-04-08 17:35:55 +0200

    ts intro

---
+++ ts_pradzia/labaspasauli.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("labas pasauli");
+let x;
+let y;
+x = 5;
+y = 6;
+console.log(x + y);
---
+++ ts_pradzia/labaspasauli.ts
@@ -0,0 +1,6 @@
+console.log("labas pasauli");
+let x: number;
+let y: number;
+x=5;
+y=6;
+console.log(x+y);
