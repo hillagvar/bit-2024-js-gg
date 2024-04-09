3a4ebc6712d829eb7b042e51beb7df53f737df9f
Hila Gvar <hilla.gvar@gmail.com>
2024-04-09 07:47:23 +0200

    ts pradzia

--- ts_pradzia/labaspasauli.js
+++ ts_pradzia/labaspasauli.js
@@ -1,7 +1,20 @@
-"use strict";
-console.log("labas pasauli");
-let x;
-let y;
-x = 5;
+console.log("Labas pasauli");
+var x;
+// Kintamasis yra skaičius, 33, 35.848, -4848
+var y;
+y = 28;
+// Tekstinis tipas, string
+var s;
+s = "tekstas";
+// Masyvas
+var skaiciuMasyvas;
+skaiciuMasyvas = [5, 9, 6];
+var tekstoMasyvas;
+tekstoMasyvas = ["a", "b"];
+// Boolean - tiesa arba melas
+var veikia;
+veikia = true;
+veikia = false;
+x = 9;
 y = 6;
 console.log(x + y);
--- ts_pradzia/labaspasauli.ts
+++ ts_pradzia/labaspasauli.ts
@@ -1,6 +1,26 @@
-console.log("labas pasauli");
-let x: number;
-let y: number;
-x=5;
+console.log("Labas pasauli");
+let x:number;
+
+// Kintamasis yra skaičius, 33, 35.848, -4848
+let y:number;
+y=0b11100;
+
+// Tekstinis tipas, string
+let s:string;
+s="tekstas";
+
+// Masyvas
+let skaiciuMasyvas:number[];
+skaiciuMasyvas=[5,9,6];
+
+let tekstoMasyvas:string[];
+tekstoMasyvas=["a", "b"];
+
+// Boolean - tiesa arba melas
+let veikia:boolean;
+veikia=true;
+veikia=false;
+
+x=9;
 y=6;
 console.log(x+y);
