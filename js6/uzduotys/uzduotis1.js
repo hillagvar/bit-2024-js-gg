af146cb4dcd72a7ec89123abe006356f9a31cbeb
Hila Gvar <hilla.gvar@gmail.com>
2024-04-12 21:40:21 +0200

    uzd1 updated

--- js6/uzduotys/uzduotis1.js
+++ js6/uzduotys/uzduotis1.js
@@ -4,10 +4,14 @@ const btn = document.getElementById("btn");
 const result = document.getElementById("result");
 
 const calcBMI = () => {
-    const height = heightInput.valueAsNumber / 100;
-    const weight = weightInput.valueAsNumber;
-    const bmi = (weight / height ** 2).toFixed(2);
-    result.innerHTML = ` ${bmi}`;
+    if (isNaN(heightInput.valueAsNumber) || isNaN(weightInput.valueAsNumber) || heightInput.valueAsNumber < 0 || weightInput.valueAsNumber < 0) {
+        result.innerHTML = ` Neteisingai įvesti duomenys`;
+    } else {
+        const height = heightInput.valueAsNumber / 100;
+        const weight = weightInput.valueAsNumber;
+        const bmi = (weight / height ** 2).toFixed(2);
+        result.innerHTML = ` ${bmi}`;
+    }
 }
 
 btn.onclick = calcBMI;
