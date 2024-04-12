872041de3095c081f1e3079c3431ed291c8e455b
Hila Gvar <hilla.gvar@gmail.com>
2024-04-12 23:37:50 +0200

    uzd update 2

--- js6/uzduotys/uzduotis1.js
+++ js6/uzduotys/uzduotis1.js
@@ -1,7 +1,23 @@
 const heightInput = document.getElementById("height");
 const weightInput = document.getElementById("weight");
 const btn = document.getElementById("btn");
+const resetBtn = document.getElementById("reset");
 const result = document.getElementById("result");
+const resultDiv = document.getElementById("resultDiv");
+
+const determineColour = (bmi) => {
+    if (bmi < 18.5) {
+        return "#AED8D0";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        return "#9BE756";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        return "#FDD805";
+    } else if (bmi >= 30 && bmi <= 34.9) {
+        return "#F68C31";
+    } else if (bmi >= 35) {
+        return "#F95353";
+    }
+}
 
 const calcBMI = () => {
     const height = heightInput.valueAsNumber / 100;
@@ -9,10 +25,20 @@ const calcBMI = () => {
 
     if (isNaN(height) || isNaN(weight) || height < 0 || weight < 0) {
         result.innerHTML = ` Neteisingai įvesti duomenys`;
+        result.style.color = "#000000";
     } else {
-        const bmi = (weight / height ** 2).toFixed(2);
+        const bmi = (weight / height ** 2).toFixed(1);
         result.innerHTML = ` ${bmi}`;
+        result.style.color = determineColour(bmi);
     }
 }
 
+const reset = () => {
+    heightInput.value = "";
+    weightInput.value = "";
+    result.innerHTML = "";
+}
+
+
 btn.onclick = calcBMI;
+resetBtn.onclick = reset;
