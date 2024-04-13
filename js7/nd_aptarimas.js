eefe0a2465a1fed64c3125acc50a469f319b85c8
Hila Gvar <hilla.gvar@gmail.com>
2024-04-13 20:46:44 +0200

    nd

---
+++ js7/nd_aptarimas.js
@@ -0,0 +1,30 @@
+let prekes = [];
+
+let pav = "Duona";
+
+let kiek = "2 vienetai";
+
+prekes.push({
+    pavadinimas: pav,
+    kiekis: kiek,
+});
+
+pav = "Cukrus";
+kiek = "1 kg";
+
+prekes.push({
+    pavadinimas: pav,
+    kiekis: kiek,
+});
+
+console.log(prekes);
+
+//norint atvaizduoti irasa
+/*
+tasks.forEach((task) => {
+        ...
+        newTask.textContent = `${task.pavadinimas} ${task.kiekis}`;
+        ...
+    });
+
+*/
