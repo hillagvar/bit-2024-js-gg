a34fde9426fc994c2be0f9edfe3ea568d2be19ed
Hila Gvar <hilla.gvar@gmail.com>
2024-04-22 02:23:05 +0200

    upd

--- ts4/uzduotis2/dist/showData.js
+++ ts4/uzduotis2/dist/showData.js
@@ -1,4 +1,5 @@
-import { showFullData } from "./showFullData.js";
+import { deleteReg } from "./deleteReg.js";
+import { updateReg } from "./updateReg.js";
 export const dataTable = document.getElementById("data-table");
 export const editForm = document.getElementById("edit-form");
 export const showData = (participantData) => {
@@ -18,6 +19,24 @@ export const showData = (participantData) => {
         tr.appendChild(tdBirthYear);
         tr.appendChild(tdV);
         dataTableBody.appendChild(tr);
-        tr.onclick = () => showFullData(p);
+        tr.onclick = () => {
+            dataTable.style.display = "none";
+            editForm.style.display = "block";
+            document.getElementById("name-edit").value = p.name;
+            document.getElementById("surname-edit").value = p.surname;
+            document.getElementById("birth-year-edit").value = p.birthYear.toString();
+            if (p.gender === "male") {
+                document.getElementById("male-edit").checked = true;
+                document.getElementById("female-edit").checked = false;
+            }
+            else {
+                document.getElementById("female-edit").checked = true;
+                document.getElementById("male-edit").checked = false;
+            }
+            document.getElementById("email-edit").value = p.email;
+            document.getElementById("phone-edit").value = p.phone;
+            document.getElementById("update-reg").onclick = () => updateReg(p);
+            document.getElementById("delete-reg").onclick = () => deleteReg(p);
+        };
     });
 };
